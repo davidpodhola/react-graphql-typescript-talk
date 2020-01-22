ac7c81985e125d41dcc4e427e6185252d190c269
David Podhola <david@podhola.net>
2020-01-22 19:43:25 +0100

    interfaces, classes

--- typescript/2/second.ts
+++ typescript/2/second.ts
@@ -1,2 +1,15 @@
-const a = 1;
+interface Second {
+  a: boolean
+  b: string
+  c: number
+  d: any
 
+  calculate(initial: string): number;
+}
+
+class SecondImplementation implements Second {
+}
+
+const inst = new SecondImplementation();
+
+console.log(inst.calculate(''));
